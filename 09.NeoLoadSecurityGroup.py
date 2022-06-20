3eea39ba4be3901ae73cd37768635210a80755d0
Fiston <60503084+Fiston-pro@users.noreply.github.com>
2022-06-20 13:23:07 +0200

    Update 09.NeoLoadSecurityGroup.py
    I changed yaml.load() function to yaml.safe_load() to avoid introducing the possibility for arbitrary code execution and also make it work.

--- 09.NeoLoadSecurityGroup.py
+++ 09.NeoLoadSecurityGroup.py
@@ -163,7 +163,7 @@ MERGE (p)<-[:Egress]-(s)'
 
 def NeoLoadAccounts(filepath):
     with open(filepath) as f:
-        accountList = yaml.load(f)
+        accountList = yaml.safe_load(f)
         for acc in accountList["accounts"]:
             print(acc['name'])
             account_id = acc['account_id']
